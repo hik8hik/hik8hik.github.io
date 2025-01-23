9b7d0d330c936a3b03dec497393dab3bf390df1b
hik8hik <hik8hik@gmail.com>
2025-01-23 20:59:25 +0000

    roll-back to see if the error is resolved by adding file .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/hik8hik.github.io",
-  assetPrefix: "/hik8hik.github.io",
-  trailingSlash: true,
-
   output: process.env.NEXT_PUBLIC_OUTPUT || undefined,
 
   images: {
