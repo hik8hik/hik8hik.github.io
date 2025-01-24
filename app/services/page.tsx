73af142cf27861ca72226556fc16d313d1fed4c1
hik8hik <hik8hik@gmail.com>
2025-01-24 03:57:16 +0000

    i services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,81 @@
+"use client";
+
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { ArrowDownRight } from "lucide-react";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "We build websites that serve as powerful marketing tools and bring memorable brand experiences.",
+  },
+  {
+    num: "02",
+    title: "Mobile Development",
+    description:
+      "We develop native and hybrid mobile apps for iOS and Android platforms.",
+  },
+  {
+    num: "03",
+    title: "UI/UX Design",
+    description: "We design user-friendly, clean and modern interfaces.",
+  },
+  {
+    num: "04",
+    title: "Prototyping",
+    description:
+      "We create interactive prototypes to validate ideas and products.",
+  },
+  {
+    num: "05",
+    title: "Quality Assurance",
+    description:
+      "We provide QA services to ensure the highest quality of our products.",
+  },
+];
+
 const Services = () => {
-  return <div>Service page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col py-12 xl:py-0 justify-center">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 gap-{60px} md:grid-cols-2 lg:grid-cols-3"
+        >
+          {services.map((service, index) => {
+            return (
+                <div key={index} className="flex-1 flex flex-col gap-6 group p-4 h-full">
+                {/* top */}
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                  {service.num}
+                  </div>
+                  <Link
+                  href="/services"
+                  className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                  <ArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                {/* heading */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+                {/* description */}
+                <p className="text-white/60">{service.description}</p>
+                {/* border */}
+                <div className="border-b border-white/20 w-full mt-auto"></div>
+                </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: process.env.NEXT_PUBLIC_OUTPUT || undefined,
 
+  /* 
+ for production
+ afte github pages refused to rednder images from sanity
+*/
   images: {
+    unoptimized: true,
+  },
+
+  /* images: {
     domains: ["localhost"],
     remotePatterns: [
       {
@@ -11,7 +22,7 @@ const nextConfig = {
         port: "",
       },
     ],
-  },
+  }, */
 };
 
 export default nextConfig;
