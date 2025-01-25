d5cdddfbe48c9ada5c0c3017dfc8cba72ea374df
hik8hik <hik8hik@gmail.com>
2025-01-25 20:58:35 +0000

    chore : remove unused-vars

--- components/WorkSliderButtons.tsx
+++ components/WorkSliderButtons.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ChevronLeft, ChevronRight, CircleArrowLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useSwiper } from "swiper/react";
 
 interface WorkSliderButtonsProps {
