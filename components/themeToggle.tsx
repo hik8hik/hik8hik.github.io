cd7552398c5b86dd532db17fd14f08cc5c93fe80
hik8hik <hik8hik@gmail.com>
2025-01-31 21:31:42 +0000

    chore: just a commit

--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -14,6 +14,7 @@ export function ModeToggle() {
     setCurrentTheme(theme);
   }, [theme]);
 
+  /* handle toggle */
   const handleToggle = () => {
     if (currentTheme === "light") {
       setTheme("dark");
